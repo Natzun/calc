6ada1ecacac8fda9cb314fe4a5b6d4fe1820e479
Natzun <natanzun@outlook.com.br>
2023-01-12 00:18:12 -0300

    Update BackEnd 1.0

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 console.log('backend');
 
+var lastInput, currentInput, resultInput;
+
+currentInput = 0;
+
+function insert(newInput){
+  currentInput = parseInt(currentInput + newInput)
+  updateDisplay();
+}
+
+function updateDisplay(){
+  const display = document.getElementById('displaySpan')
+  display.innerText = currentInput;
+}
+
 function add(a = required(), b = required()){
   return a + b;
 }
