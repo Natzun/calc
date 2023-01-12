570e58b1e9b47f4c73019282751445523fd39d38
Natzun <natanzun@outlook.com.br>
2023-01-11 23:43:58 -0300

    Update Base Function 1.0

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('backend');
+
+function add(a = required(), b = required()){
+  return a + b;
+}
+
+function multiply(a = required(), b = required()){
+  return a * b;
+}
+
+function subtract(a = required(), b = required()){
+  return a - b;
+}
+
+function divide(a = required(), b = required()){
+  return a / b;
+}
